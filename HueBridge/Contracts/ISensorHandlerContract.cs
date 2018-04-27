00d93576bab65819746c788952da7e58f25703a1
Zhu Zhihao <Zhu.Zhihao@bizerba.com>
2018-04-27 22:13:58 +0800

    add skeleton for sensors

---
+++ HueBridge/Contracts/ISensorHandlerContract.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HueBridge.Models;
+
+namespace HueBridge
+{
+    interface ISensorHandlerContract
+    {
+        List<string> SupportedModels { get; }
+        Task<List<Sensor>> ScanSensors(string hostIP);
+        Task<Sensor> GetSensorState(Sensor sensor);
+        bool ConfigSensor(Sensor sensor);
+    }
+}
---
+++ HueBridge/Models/Sensor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HueBridge.Models
+{
+    public class Sensor
+    {
+        public Dictionary<string, object> State { get; set; }
+        public Dictionary<string, object> Config { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string ModelId { get; set; }
+        public string ManufacturerName { get; set; }
+        public string SWVersion { get; set; }
+        public string UniqueId { get; set; }
+    }
+}
